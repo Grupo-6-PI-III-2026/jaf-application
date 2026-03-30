1f5dc44a50fe36b6f46b571bb015ac8fd00a3add
Gui-DoSantos <guilherme.santos061@sptech.school>
2026-03-29 23:24:01 -0300

    feat: estrutura do html e css feita

--- jaf-frontend/react/src/routes.tsx
+++ jaf-frontend/react/src/routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Pages/Login/Login";
 
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
 
         
         <Route path="*" element={<Navigate to="/" />} />
