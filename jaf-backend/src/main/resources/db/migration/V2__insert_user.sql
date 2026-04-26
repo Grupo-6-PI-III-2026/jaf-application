073555d6d73d788d11aa7dca7d980893a8684521
GuiAndradeGap <gapguilherme.81@gmail.com>
2026-04-26 12:45:02 -0300

    Atualiza hash da senha do admin para BCrypt

--- jaf-backend/src/main/resources/db/migration/V2__insert_user.sql
+++ jaf-backend/src/main/resources/db/migration/V2__insert_user.sql
@@ -1,2 +1,2 @@
 INSERT INTO funcionario (nome, email, senha, cargo_global) 
-VALUES ('Administrador', 'admin@gmail.com', 'Admin@123', 'ADMIN');
+VALUES ('Administrador', 'admin@gmail.com', '$2a$10$yPsrDB3hR4NRGdCKaTxrz.CNjwgz2LjEZnUMw9ttY00GHSDfhLcdW', 'ADMIN');
